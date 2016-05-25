08d4882bff7d4e6463e73c298e2590a9f9c7e9d0
William Oliphant <will_oliphant@hotmail.com>
2016-05-25 09:39:41 -0400

    Added a check to see if a user is logged on before proceeding with adding items to a tray

--- src/ASPNetExercises/Controllers/TrayController.cs
+++ src/ASPNetExercises/Controllers/TrayController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Http;
 using ASPNetExercises.Utils;
+using ASPNetExercises.Models;
 
 namespace ASPNetExercises.Controllers
 {
     public class TrayController : Controller
     {
+        AppDbContext _db;
+        public TrayController(AppDbContext context)
+        {
+            _db = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -15,6 +23,39 @@ namespace ASPNetExercises.Controllers
         {
             HttpContext.Session.Remove("tray"); // clear out current tray
             HttpContext.Session.SetString(SessionVars.Message, "Tray Cleared"); // clear out current cart once order has been placed
+            return Redirect("/Home");
+        }
+
+        // Add the tray, pass the session variable info to the db
+        public ActionResult AddTray()
+        {
+            // they can't add a Tray if they're not logged on
+            if (HttpContext.Session.GetString(SessionVars.User) == null)
+            {
+                return Redirect("/Login");
+            }
+            TrayModel model = new TrayModel(_db);
+            int retVal = -1;
+            string retMessage = "";
+            try
+            {
+                Dictionary<string, object> trayItems = HttpContext.Session.GetObject<Dictionary<string, object>>(SessionVars.Tray);
+                retVal = model.AddTray(trayItems, HttpContext.Session.GetString(SessionVars.User));
+                if (retVal > 0) // Tray Added
+                {
+                    retMessage = "Tray " + retVal + " Created!";
+                }
+                else // problem
+                {
+                    retMessage = "Tray not added, try again later";
+                }
+            }
+            catch (Exception ex) // big problem
+            {
+                retMessage = "Tray was not created, try again later! - " + ex.Message;
+            }
+            HttpContext.Session.Remove(SessionVars.Tray); // clear out current tray once persisted
+            HttpContext.Session.SetString(SessionVars.Message, retMessage);
             return Redirect("/Home");
         }
     }
---
+++ src/ASPNetExercises/Models/TrayModel.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Entity;
+using ASPNetExercises.ViewModels;
+
+namespace ASPNetExercises.Models
+{
+    public class TrayModel
+    {
+        private AppDbContext _db;
+        public TrayModel(AppDbContext ctx)
+        {
+            _db = ctx;
+        }
+        public int AddTray(Dictionary<string, object> items, string user)
+        {
+            int trayId = -1;
+            using (_db)
+            {
+                // we need a transaction as multiple entities involved
+                using (var _trans = _db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Tray tray = new Tray();
+                        tray.UserId = user;
+                        tray.DateCreated = System.DateTime.Now;
+                        tray.TotalCalories = 0;
+                        tray.TotalCholesterol = 0;
+                        tray.TotalFat = 0;
+                        tray.TotalFibre = 0;
+                        tray.TotalSalt = 0;
+                        tray.TotalProtein = 0;
+                        // calculate the totals and then add the tray row to the table
+                        foreach (var key in items.Keys)
+                        {
+                            MenuItemViewModel item = JsonConvert.DeserializeObject<MenuItemViewModel>(Convert.ToString(items[key]));
+                            if (item.Qty > 0)
+                            {
+                                tray.TotalCalories += item.CAL * item.Qty;
+                                tray.TotalCholesterol += item.CHOL * item.Qty;
+                                tray.TotalFat += item.FAT * item.Qty;
+                                tray.TotalFibre += item.FBR * item.Qty;
+                                tray.TotalSalt += item.SALT * item.Qty;
+                                tray.TotalProtein += item.PRO * item.Qty;
+                            }
+                        }
+                        _db.Trays.Add(tray);
+                        _db.SaveChanges();
+                        // then add each item to the trayitems table
+                        foreach (var key in items.Keys)
+                        {
+                            MenuItemViewModel item = JsonConvert.DeserializeObject<MenuItemViewModel>(Convert.ToString(items[key]));
+                            if (item.Qty > 0)
+                            {
+                                TrayItem tItem = new TrayItem();
+                                tItem.Qty = item.Qty;
+                                tItem.MenuItemId = item.Id;
+                                tItem.TrayId = tray.Id;
+                                _db.TrayItems.Add(tItem);
+                                _db.SaveChanges();
+                            }
+                        }
+                        // test trans by uncommenting out these 3 lines
+                        //int x = 1;
+                        //int y = 0;
+                        //x = x / y;
+                        _trans.Commit();
+                        trayId = tray.Id;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        _trans.Rollback();
+                    }
+                }
+            }
+            return trayId;
+        }
+    }
+}
